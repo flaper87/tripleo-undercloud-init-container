88151a662921afec8a46a295605e6b1362639a6a
Flavio Percoco <flaper87@gmail.com>
2017-02-09 11:14:35 +0100

    not all those packages are required

--- install_packages.sh
+++ install_packages.sh
@@ -3,24 +3,10 @@
 set -ex
 
 yum install -y \
-    glibc-common \
-    initscripts \
-    systemd \
     git \
-    dhclient \
-    ethtool \
-    jq \
     vim \
     sudo \
-    docker \
-    git-review \
-    openstack-heat-api \
-    openstack-heat-engine \
-    python-heat-agent-docker-cmd \
-    python-heat-agent-hiera \
-    python-heat-agent-apply-config \
-    python-heat-agent-puppet \
-    python-ipaddr python-tripleoclient
+    docker
 
 # Remove unnecessary packages
 yum autoremove -y
