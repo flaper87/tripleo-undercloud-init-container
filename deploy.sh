213f99e86e0366f59998743482f70838d8d91336
Flavio Percoco <flaper87@gmail.com>
2017-02-10 12:25:28 +0100

    Install some services for now

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,20 @@ pushd undercloud_containers
 git checkout -b update-with-docker-puppet-dir origin/update-with-docker-puppet-dir
 sh doit.sh
 popd
+
+
+# NOTE(flaper87): Install only a couple of services for now
+cat > tripleo-heat-templates/roles_data_undercloud.yaml <<-EOF_CAT
+- name: Undercloud # the 'primary' role goes first
+  CountDefault: 1
+  disable_constraints: True
+  ServicesDefault:
+    - OS::TripleO::Services::MySQL
+    - OS::TripleO::Services::MongoDb
+    - OS::TripleO::Services::Keystone
+    - OS::TripleO::Services::Apache
+    - OS::TripleO::Services::GlanceApi
+EOF_CAT
+
+
 sh run.sh
