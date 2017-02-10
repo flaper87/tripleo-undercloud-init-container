52fbef596e57ab6bfd5013270613e2a7c690ad9e
Flavio Percoco <flaper87@gmail.com>
2017-02-10 12:23:54 +0100

    missing paths

--- run.sh
+++ run.sh
@@ -4,12 +4,8 @@ set -ex
 
 docker run --name undercloud-deploy --net=host --privileged \
 -v /tmp:/tmp \
--v /var/lib/docker-puppet.json:/var/lib/docker-puppet.json \
--v /var/lib/docker-puppet-tasks1.json:/var/lib/docker-puppet-tasks1.json \
--v /var/lib/docker-puppet-tasks2.json:/var/lib/docker-puppet-tasks2.json \
--v /var/lib/docker-puppet-tasks3.json:/var/lib/docker-puppet-tasks3.json \
--v /var/lib/docker-puppet-tasks4.json:/var/lib/docker-puppet-tasks4.json \
--v /var/lib/docker-puppet-tasks5.json:/var/lib/docker-puppet-tasks5.json \
+-v /etc/puppet:/etc/puppet \
+-v /var/lib/kolla:/var/lib/kolla \
 -v /var/lib/docker-puppet:/var/lib/docker-puppet \
 -v /var/lib/config-data:/var/lib/config-data \
 -v /var/run/docker.sock:/var/run/docker.sock \
